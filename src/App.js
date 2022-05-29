5ceeb25e47361f8a598e704b4d7811d643aaee09
Ave Nurme <nurme.ave@gmail.com>
2022-05-29 19:37:59 +0300

    add useState, render boxes onto the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+import boxes from './boxes'
 import './App.css';
 
 function App() {
+  const [squares, setSquares] = useState(boxes)
+
+  const squaresElements = squares.map((square) => {
+    return <div className="box" key={square.id}></div>
+  })
+
   return (
     <main className="main-container">
-      <h1>Simple test</h1> 
+      { squaresElements }
     </main>
   );
 }
